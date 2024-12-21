da45aa557d59b3374eec241879970e9aadb0432d
shuanvi7 <shuanvi7@gmail.com>
2024-12-21 21:26:10 +0300

    Test scrollTo

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,37 @@
 <template>
-  <div>Vue 3</div>
+  <ul class="list">
+    <li :class="item.class" v-for="item in data.todoList" :key="item">
+      <div>{{ item.text }} {{ item.class }}</div>
+    </li>
+  </ul>
 </template>
 
 <script lang="ts">
+import './App.css'
+import { data } from './mockData.js'
+
 export default {
   data() {
-    return {}
+    return {
+      positionScroll: 0,
+      data,
+    }
+  },
+  mounted() {
+    this.scrollPosition()
+  },
+  methods: {
+    scrollPosition() {
+      const list = this.$el
+      const activeItem = this.$el.querySelector('.active')
+
+      const positionActiveItem = activeItem.offsetTop
+      const positionList = this.$el.offsetTop
+      const summuryPosition = positionActiveItem - positionList
+
+      console.log(summuryPosition)
+      list.scrollTo(0, summuryPosition)
+    },
   },
 }
 </script>
---
+++ src/mockData.js
@@ -0,0 +1,88 @@
+export const data = {
+  todoList: [
+    {
+      text: 'One',
+      class: '',
+    },
+    {
+      text: 'Two',
+      class: '',
+    },
+    {
+      text: 'Three',
+      class: '',
+    },
+    {
+      text: 'Four',
+      class: '',
+    },
+    {
+      text: 'Five',
+      class: '',
+    },
+    {
+      text: 'Six',
+      class: '',
+    },
+    {
+      text: 'Seven',
+      class: '',
+    },
+    {
+      text: 'Eight',
+      class: '',
+    },
+    {
+      text: 'Nine',
+      class: 'active',
+    },
+    {
+      text: 'Ten',
+      class: '',
+    },
+    {
+      text: 'Ten',
+      class: '',
+    },
+    {
+      text: 'Ten',
+      class: '',
+    },
+    {
+      text: 'Ten',
+      class: '',
+    },
+    {
+      text: 'Ten',
+      class: '',
+    },
+    {
+      text: 'Ten',
+      class: '',
+    },
+    {
+      text: 'Ten',
+      class: '',
+    },
+    {
+      text: 'Ten',
+      class: '',
+    },
+    {
+      text: 'Ten',
+      class: '',
+    },
+    {
+      text: 'Ten',
+      class: '',
+    },
+    {
+      text: 'Ten',
+      class: '',
+    },
+    {
+      text: 'Ten',
+      class: '',
+    },
+  ],
+};
