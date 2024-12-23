112bd40d6316294d5dd67709f70438f865c88104
shuanvi7 <shuanvi7@gmail.com>
2024-12-23 21:57:51 +0300

    Разработка проекта

---
+++ src/app/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <TheHeader />
+</template>
+
+<script lang="ts">
+import './styles/fonts.css'
+import './styles/global.css'
+import TheHeader from '@/widgets/header/ui/TheHeader.vue'
+
+export default {
+  data() {
+    return {}
+  },
+  components: {
+    TheHeader,
+  },
+}
+</script>
---
+++ src/widgets/header/ui/TheHeader.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>Header</div>
+</template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {}
+  },
+}
+</script>
+<style lang=""></style>
